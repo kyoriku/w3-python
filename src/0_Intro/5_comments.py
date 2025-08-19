0d4ad8871101442be606bdb0cc7383b3d6c43987
Austin Graham <austiingraham@gmail.com>
2025-08-19 17:16:37 -0400

    add intro tutorial files: hello world, variables, comments, indentation

---
+++ src/0_Intro/5_comments.py
@@ -0,0 +1,7 @@
+# This is a comment
+print("Hello, World!")
+
+print("Python is fun!")  # This comment is after a statement
+
+# print("Hello, World!") - preventing code execution
+print("Cheers, Mate!")
