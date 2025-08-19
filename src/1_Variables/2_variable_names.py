e181f8c849e451b41b5975047f5ac2f67601c820
Austin Graham <austiingraham@gmail.com>
2025-08-19 19:01:25 -0400

    add variables files: creation, naming, multiple values, output, global scope

---
+++ src/1_Variables/2_variable_names.py
@@ -0,0 +1,17 @@
+# Variables names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Multi Words Variable Names
+# Camel Case
+myVariableName = "John"
+
+# Pascal Case
+MyVariableName = "John"
+
+# Snake Case
+my_variable_name = "John"
