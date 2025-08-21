f145679b585ab83a286cb220c40447d464ce0b9c
Austin Graham <austiingraham@gmail.com>
2025-08-20 21:09:33 -0400

    add data type files: basic types, numbers, casting

---
+++ src/2_Data_Types_Numbers_Casting/4_exercises.py
@@ -0,0 +1,64 @@
+# If x = 5, what is a correct syntax for printing the data type of the variable x?
+print(type(x))
+
+# The following code example would print the data type of x, what data type would that be?
+# x = 5
+# print(type(x))
+int
+
+# The following code example would print the data type of x, what data type would that be?
+# x = "Hello World"
+# print(type(x))
+str
+
+# The following code example would print the data type of x, what data type would that be?
+# x = 20.5
+# print(type(x))
+float
+
+# The following code example would print the data type of x, what data type would that be?
+# x = ["apple", "banana", "cherry"]
+# print(type(x))
+list
+
+# The following code example would print the data type of x, what data type would that be?
+# x = ("apple", "banana", "cherry")
+# print(type(x))
+tuple
+
+# The following code example would print the data type of x, what data type would that be?
+# x = {"name" : "John", "age" : 36}
+# print(type(x))
+dict
+
+# The following code example would print the data type of x, what data type would that be?
+# x = True
+# print(type(x))
+bool
+
+# Which is NOT a legal numeric data type in Python:
+long
+
+# Insert the correct syntax to convert x into a floating point number.
+x = 5
+x = float(x)
+
+# Insert the correct syntax to convert x into a integer.
+x = 5.5
+x = int(x)
+
+# Insert the correct syntax to convert x into a complex number.
+x = 5
+x = complex(x)
+
+# What will be the result of the following code:
+# print(int(35.88))
+35
+
+# What will be the result of the following code:
+# print(float(35))
+35.0
+
+# What will be the result of the following code:
+# print(str(35.82))
+35.82
