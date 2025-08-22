82876c1cc48214b41c474fcfcde845bcb68a763f
Austin Graham <austiingraham@gmail.com>
2025-08-22 18:11:53 -0400

    add operator files: booleans, arithmetic, assignment, comparison, logical, identity, membership, bitwise

---
+++ src/4_Booleans_Operators/3_assignment_operators.py
@@ -0,0 +1,66 @@
+# Variable Assignment
+x = 5
+print(x)
+
+# Addition Assignment
+x = 5
+x += 3
+print(x)
+
+# Subtraction Assignment
+x = 5
+x -= 3
+print(x)
+
+# Multiplication Assignment
+x = 5
+x *= 3
+print(x)
+
+# Division Assignment
+x = 5
+x /= 3
+print(x)
+
+# Modulus Assignment
+x = 5
+x %= 3
+print(x)
+
+# Floor Division Assignment
+x = 5
+x //= 3
+print(x)
+
+# Exponentiation Assignment
+x = 5
+x **= 3
+print(x)
+
+# Bitwise AND Assignment
+x = 5
+x &= 3
+print(x)
+
+# Bitwise OR Assignment
+x = 5
+x |= 3
+print(x)
+
+# Bitwise XOR Assignment
+x = 5
+x ^= 3
+print(x)
+
+# Bitwise Right Shift Assignment
+x = 5
+x >>= 3
+print(x)
+
+# Bitwise Left Shift Assignment
+x = 5
+x <<= 3
+print(x)
+
+# Walrus Operator
+print(x := 3)
