e69d6cecb882d1722fc184ad14b8044e0d1bec87
Austin Graham <austiingraham@gmail.com>
2025-09-08 23:08:37 -0400

    add dictionaries files: creation, access, modification, addition, removal, looping, copying, nesting

---
+++ src/8_Dictionaries/exercises/2_access_dictionaries.py
@@ -0,0 +1,16 @@
+# True or False. You can access item values by referring to the key name.
+True
+
+# Use the get method to print the value of the "model" key of the car dictionary.
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+# Consider the following code:
+# x = {'type' : 'fruit', 'name' : 'banana'}
+# print(x['type'])
+# What will be the printed result?
+fruit
