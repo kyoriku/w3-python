fdce9f8ef3ff2685fa688b7ebb8365c334be6fe7
Austin Graham <austiingraham@gmail.com>
2025-09-17 17:05:49 -0400

    reorganize data types, numbers & casting section: create tutorials, exercises, challenges subdirectories

---
+++ src/2_Data_Types_Numbers_Casting/challenges/3_numbers.py
@@ -0,0 +1,14 @@
+# Use the type() function to print the data types of the three variables.
+
+# Requirements
+# Print the data type of variable `x`.
+# Print the data type of variable `y`.
+# Print the data type of variable `z`.
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
