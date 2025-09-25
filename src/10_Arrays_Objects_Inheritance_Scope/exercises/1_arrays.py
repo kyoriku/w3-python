f819ff444cfdd2935b7d06b528da339276e4879e
Austin Graham <austiingraham@gmail.com>
2025-09-24 21:16:16 -0400

    add programming fundamentals files: arrays, classes, inheritance, scope

---
+++ src/10_Arrays_Objects_Inheritance_Scope/exercises/1_arrays.py
@@ -0,0 +1,13 @@
+# Python Lists can be used as arrays.
+# What will be the result of the following code:
+# fruits = ['apple', 'banana', 'cherry']
+# print(fruits[0])
+apple
+
+# What is a correct Python List method used to return the number of elements in a list?
+len()
+
+# What will be the result of the following code:
+# fruits = ['apple', 'bananan', 'cherry']
+# print(len(fruits))
+3
---
+++ src/10_Arrays_Objects_Inheritance_Scope/exercises/3_inheritance.py
@@ -0,0 +1,19 @@
+# What is the correct keyword to use inside an empty class, to avoid getting an error?
+pass
+
+# What is the correct syntax to create a class named Student that will inherit properties and methods from a class named Person?
+class Student(Person):
+
+# We have used the Student class to create an object named x. What is the correct syntax to execute the printname method of the object x?
+class Person:
+  def __init__(self, fname):
+    self.firstname = fname
+
+  def printname(self):
+    print(self.firstname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike")
+x.printname()
